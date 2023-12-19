0e91da7c7c70018105e2aac3ecba9446c30c9143
Hyun Soo An <ahs20103@gmail.com>
2023-12-19 21:33:35 +0900

    20231219_게임 만들기

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -31,14 +32,22 @@ public class PlayerController : MonoBehaviour
     void UpdateMoving()
     {
         Vector3 dir = _destPos - transform.position;
-        if (dir.magnitude < 0.0001f)
+        if (dir.magnitude < 0.01f)
         {
             _state = PlayerState.Idle;
         }
         else
         {
+            NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
             float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-            transform.position += dir.normalized * moveDist;
+            nma.Move(dir.normalized * moveDist);
+
+            Debug.DrawRay(transform.position + Vector3.up * 0.5f, dir.normalized, Color.green);
+            if(Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+                _state = PlayerState.Idle;
+                return;
+            }
 
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
         }
@@ -57,7 +66,7 @@ public class PlayerController : MonoBehaviour
 
     void OnRunEvent()
     {
-        Debug.Log("¶Ñ¹÷ ¶Ñ¹÷");
+        //Debug.Log("¶Ñ¹÷ ¶Ñ¹÷");
     }
     void Update()
     {
@@ -79,7 +88,8 @@ public class PlayerController : MonoBehaviour
     {
         if (_state == PlayerState.Die) { return; }
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        
+        //Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit,100.0f,LayerMask.GetMask("Wall"))) 
